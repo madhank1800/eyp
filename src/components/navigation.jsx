711f332ff224339ff8318f9101c786edda5fd9a7
madhank1800 <madhankumar1800@gmail.com>
2023-12-27 23:14:48 +0530

    intial

---
+++ src/components/navigation.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import logo from '../assests/images/eyp02.jpeg'
+export const Navigation = (props) => {
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+          </button>
+          {/* <a className="navbar-brand page-scroll" href="#page-top">
+            React Landing Page
+          </a>{" "} */}
+          <img src={logo} alt="logo" width={70}/>
+        </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+         
+          <li>
+              <a href="#about" className="page-scroll">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="page-scroll">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#features" className="page-scroll">
+                Features
+              </a>
+            </li>
+           
+            
+            <li>
+              <a href="#portfolio" className="page-scroll">
+                Industries
+              </a>
+            </li>
+            <li>
+              <a href="#testimonials" className="page-scroll">
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a href="#team" className="page-scroll">
+                Team
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="page-scroll">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a href="#" className="page-scroll">
+                Sign In
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
